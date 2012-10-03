7423e24f2923de1c9039c6338a0263da26604226
Andres Arellano <andres.arellano@gmail.com>
2012-10-03 19:33:25 -0400

    Small fixes to tc-gen-random

--- guitar-scripts/tc-gen-random.sh
+++ guitar-scripts/tc-gen-random.sh
@@ -7,10 +7,9 @@ function usage {
         <tc-dir> [-D no-duplicate-event] [-T treat-terminal-event-specially]"
 }
 
-base_dir=`dirname $0`
-guitar_lib=$base_dir/jars
+guitar_lib=$guitar_lib/jars
 
-# Main classes 
+# Main classes
 tcgen_launcher=edu.umd.cs.guitar.testcase.TestCaseGenerator
 
 for file in `find $guitar_lib/ -name "*.jar"`
@@ -18,13 +17,8 @@ do
 	guitar_classpath=${file}:${guitar_classpath}
 done
 
-# Change GUITAR_OPTS variable to run with the clean log file  
-GUITAR_OPTS="$GUITAR_OPTS -Dlog4j.configuration=log/guitar-clean.glc"
-
-if [ -z "$JAVA_CMD_PREFIX" ];
-then
-    JAVA_CMD_PREFIX="java"
-fi
+# Change guitar_opts variable to run with the clean log file
+guitar_opts="$guitar_opts -Dlog4j.configuration=log/guitar-clean.glc"
 
 plugin=RandomSequenceLengthCoverage
 
@@ -35,8 +29,7 @@ then
 	classpath=`cygpath -wp $classpath`
 fi
 
-TCGEN_CMD="$JAVA_CMD_PREFIX $GUITAR_OPTS -cp $classpath $tcgen_launcher -p $plugin $@"
-$TCGEN_CMD
+java $guitar_opts -cp $classpath $tcgen_launcher -p $plugin $@
 
 
 
