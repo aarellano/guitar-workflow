9bd0d32952d60f163ecb6683534e58aeb5dc3179
Andres Arellano <andres.arellano@gmail.com>
2012-10-19 18:03:09 -0400

    Reimplements the ruby script using nokogiri instead of xmlsimple

--- util/matrix-gen.rb
+++ util/matrix-gen.rb
@@ -1,25 +1,26 @@
 require 'mysql'
-require 'xmlsimple'
+require 'nokogiri'
 
 begin
-dbh = Mysql.real_connect("localhost", "root", "root", "cs737")
+  dbh = Mysql.real_connect("localhost", "root", "root", "cs737")
 rescue Mysql::Error => e
   puts "Error code: #{e.errno}"
   puts "Error message: #{e.error}"
   puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
 end
 
+table = "drjava"
 dbh.query("DROP TABLE IF EXISTS drjava")
 dbh.query("CREATE TABLE drjava(package VARCHAR(100), class VARCHAR(100), line INT, hits INT, primary key (package, class, line))")
 
-xml = XmlSimple.xml_in('coverage.xml', { 'KeyAttr' => 'name' })
-table = "drjava"
-xml["packages"][0]["package"].each do |package, p_row|
-  p_row["classes"][0]["class"].each do |c_name, c_row|
-    c_row["lines"][0].each do |line, l_row|
-      l_row.each do |info|
-        dbh.query "INSERT INTO " + table + " (package, class, line, hits) VALUES('" + package + "','" + c_name + "'," + info["number"] + "," + info["hits"] + ");"
-      end
+f = File.open("coverage.xml")
+xml = Nokogiri::XML(f)
+f.close
+
+xml.xpath("//packages/package").each do |p|
+  p.xpath("classes/class").each do |c|
+    c.xpath("lines/line").each do |l|
+      dbh.query "INSERT INTO " + table + " (package, class, line, hits) VALUES('" + p["name"] + "','" + c["name"] + "'," + l["number"] + "," + l["hits"] + ");"
     end
   end
 end
